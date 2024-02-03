45092f19a3b2ffe6769d0923a21396f165c20114
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2024-02-03 12:55:34 +0100

    Change 'python' commands to 'python3'
    The reason why we do that is to ensure that the application is running
    on python3. There can be some wierd linking issue when running the
    'python' command that can cause that to run on python v2. So this should
    be fixed with this change.

--- control.sh
+++ control.sh
@@ -1,14 +1,14 @@
 if [ $1 = "init" ]; then
 
-    if [ -f "/tmp/minitwit.db" ]; then 
+    if [ -f "/tmp/minitwit.db" ]; then
         echo "Database already exists."
         exit 1
     fi
     echo "Putting a database to /tmp/minitwit.db..."
-    python -c"from minitwit import init_db;init_db()"
+    python3 -c "from minitwit import init_db;init_db()"
 elif [ $1 = "start" ]; then
     echo "Starting minitwit..."
-    nohup `which python` minitwit.py > /tmp/out.log 2>&1 &
+    nohup `which python3` minitwit.py > /tmp/out.log 2>&1 &
 elif [ $1 = "stop" ]; then
     echo "Stopping minitwit..."
     pkill -f minitwit
@@ -19,5 +19,3 @@ elif [ $1 = "flag" ]; then
 else
   echo "I do not know this command..."
 fi
-
-
