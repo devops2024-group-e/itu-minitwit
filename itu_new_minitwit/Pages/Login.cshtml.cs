fca7816091183c1ceb172842ab093b9413a10b07
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2024-02-09 22:03:33 +0100

    Add working in memory login

---
+++ itu_new_minitwit/Pages/Login.cshtml.cs
@@ -0,0 +1,47 @@
+using itu_new_minitwit.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
+
+namespace itu_new_minitwit.Pages;
+
+public class LoginModel : PageModel
+{
+    [BindProperty]
+    public string Username { get; set; }
+
+    [BindProperty]
+    public string Password { get; set; }
+
+    public string ErrorMessage { get; set; } = default!;
+
+    private readonly List<UserCredential> _users = new()
+    {
+        new UserCredential { Username = "Testuser", Password = "sesam123" }
+    };
+
+    public void OnGet()
+    {
+    }
+
+    public IActionResult OnPost()
+    {
+        var user = _users.SingleOrDefault(x => x.Username == Username);
+        if (user == null)
+        {
+            ErrorMessage = "Invalid username or password";
+            return Page();
+        }
+
+
+        if (user.Password == Password)
+        {
+            return RedirectToPage("/Index");
+        }
+        else
+        {
+            ErrorMessage = "Invalid username or password";
+            return Page();
+        }
+    }
+}
