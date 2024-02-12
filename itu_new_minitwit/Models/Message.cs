0d39c16565b8fd083c89a37dc87804232f36bf5a
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2024-02-12 11:01:42 +0100

    Scaffold C# database code

---
+++ itu_new_minitwit/Models/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace itu_new_minitwit.Models;
+
+public partial class Message
+{
+    public long MessageId { get; set; }
+
+    public long AuthorId { get; set; }
+
+    public byte[] Text { get; set; } = null!;
+
+    public long? PubDate { get; set; }
+
+    public long? Flagged { get; set; }
+}
---
+++ itu_new_minitwit/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace itu_new_minitwit.Models;
+
+public partial class User
+{
+    public long UserId { get; set; }
+
+    public byte[] Username { get; set; } = null!;
+
+    public byte[] Email { get; set; } = null!;
+
+    public byte[] PwHash { get; set; } = null!;
+}
