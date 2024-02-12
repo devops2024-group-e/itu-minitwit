e259a8c602c21dc5dc600ed4f5473057317e9121
amalieboegild <abso@itu.dk>
2024-02-12 11:41:14 +0100

    add db contect to index

--- itu_new_minitwit/Pages/Index.cshtml.cs
+++ itu_new_minitwit/Pages/Index.cshtml.cs
@@ -13,23 +13,27 @@ public class IndexModel : PageModel
 
     public string Title { get; set; }
 
+    private MinitwitContext _context;
 
     public List<Message> Messages { get; set; } = new List<Message>{
         new Message{Username = "Amalie", Text = "Helloooooo Hanne", PublishedDate = DateTime.Now },
         new Message{Username = "Malin", Text = "Who is Hanne?", PublishedDate = DateTime.Now.AddMinutes(1)}
     };
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, MinitwitContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public void follow()
+    public void follow(User userToFollow)
     {
         // TODO: Create a query to follow a specific user
+        var ownUserID = HttpContext.Session.GetInt32("user_id");
+        
     }
 
-    public void unfollow()
+    public void unfollow(User userToUnfollow)
     {
         // TODO: Create a query to unfollow a specific user
     }
