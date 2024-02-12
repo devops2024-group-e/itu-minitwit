cc9fc42575710ef7b082183d5b0d9868002c0795
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2024-02-12 11:27:28 +0100

    Add todos of which queries to create

--- itu_new_minitwit/Pages/Index.cshtml.cs
+++ itu_new_minitwit/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using itu_new_minitwit.Model;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
 
 namespace itu_new_minitwit.Pages;
 
@@ -8,10 +9,10 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
 
-    public string Timeline{ get; set; }
+    public string Timeline { get; set; }
 
-    public string Title{ get; set; }
-    
+    public string Title { get; set; }
+
 
     public List<Message> Messages { get; set; } = new List<Message>{
         new Message{Username = "Amalie", Text = "Helloooooo Hanne", PublishedDate = DateTime.Now },
@@ -23,9 +24,23 @@ public class IndexModel : PageModel
         _logger = logger;
     }
 
+    public void follow()
+    {
+        // TODO: Create a query to follow a specific user
+    }
+
+    public void unfollow()
+    {
+        // TODO: Create a query to unfollow a specific user
+    }
+
     public void OnGet(string title)
-    {   
-        if(title == null)
+    {
+        // TODO: If we do not have a user in the session query public timeline
+        // TODO: Add query for public timeline
+
+        // TODO: Else get timeline from the users follow list
+        if (title == null)
         {
             Title = "My";
         }
@@ -37,6 +52,6 @@ public class IndexModel : PageModel
 
     public void OnPost()
     {
-        Messages.Add(new Message{Username = "My", Text = Request.Form["message"], PublishedDate = DateTime.Now});
+        Messages.Add(new Message { Username = "My", Text = Request.Form["message"], PublishedDate = DateTime.Now });
     }
 }
