20690c4f628e38d15a59837bdc0f83739fcd10fa
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2024-02-29 15:49:29 +0100

    Add docker compose file and create new connectionstrings

--- MinitwitSimulatorAPI/Program.cs
+++ MinitwitSimulatorAPI/Program.cs
@@ -11,9 +11,11 @@ builder.Configuration
     .AddEnvironmentVariables(prefix: "Minitwit_");
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddDbContext<MinitwitContext>(opt =>
-    opt.UseSqlite("Data source=../test-tmp/minitwit.db"));
+builder.Services.AddDbContext<MinitwitContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("MinitwitDatabase"));
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
