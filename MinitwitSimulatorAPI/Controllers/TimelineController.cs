8861150e73336245d57facbe4dccdb587c648f8a
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2024-03-01 07:48:56 +0100

    Change message endpoints to return correct format and accept no parameter

--- MinitwitSimulatorAPI/Controllers/TimelineController.cs
+++ MinitwitSimulatorAPI/Controllers/TimelineController.cs
@@ -42,15 +42,15 @@ public class TimelineController : Controller
     /// </summary>
     /// <param name="ownUsername">The username of the user to be followed.</param>
     /// <returns>Either Http code 404 (NotFound) or Http code 204 (Nocontent)</returns>
-    /// 
+    ///
     [HttpPost("/fllws/{username}")]
-    public async Task<IActionResult> FollowUnfollowUser([FromQuery]int latest, string username)
+    public async Task<IActionResult> FollowUnfollowUser([FromQuery] int latest, string username)
     {
- 
+
         LatestDBUtils.UpdateLatest(_context, latest);
 
         var ownUserId = GetUser(username).UserId;
-        if (!IsLoggedIn()){ return NotFound(); } // maybe should be Unauthorized();
+        if (!IsLoggedIn()) { return NotFound(); } // maybe should be Unauthorized();
 
         string otherUsername = "";
         string action = "follow";
@@ -59,7 +59,7 @@ public class TimelineController : Controller
             string jsonstring = await reader.ReadToEndAsync();
             string command = jsonstring.Split(':')[0];
             var dict = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, string>>(jsonstring);
-            if (command.Contains("unfollow")) 
+            if (command.Contains("unfollow"))
             {
                 action = "unfollow";
             }
@@ -67,8 +67,9 @@ public class TimelineController : Controller
         }
         var otherUserId = GetUser(otherUsername).UserId;
 
-        string query  = "INSERT INTO follower (who_id, whom_id) VALUES ({0}, {1})";
-        if (action.Equals("unfollow")) {
+        string query = "INSERT INTO follower (who_id, whom_id) VALUES ({0}, {1})";
+        if (action.Equals("unfollow"))
+        {
             query = "DELETE FROM follower WHERE who_id = {0} AND whom_id = {1}";
         }
 
@@ -82,12 +83,12 @@ public class TimelineController : Controller
     /// <param name="username">The user whose message is to be posted</param>
     /// <returns>Either Http code 404 (NotFound) or Http code 204 (Nocontent)</returns>
     [HttpPost("/msgs/{username}")]
-    public async Task<IActionResult> AddMessage([FromQuery]int latest, string username)
+    public async Task<IActionResult> AddMessage([FromQuery] int latest, string username)
     {
-        
+
         LatestDBUtils.UpdateLatest(_context, latest);
 
-        if (!IsLoggedIn()){ return NotFound(); } // maybe should be Unauthorized();
+        if (!IsLoggedIn()) { return NotFound(); } // maybe should be Unauthorized();
         string text = "";
         using (StreamReader reader = new StreamReader(HttpContext.Request.Body))
         {
@@ -114,18 +115,18 @@ public class TimelineController : Controller
     /// <param name="username">The username of the user, whose messages should be returned.</param>
     /// <returns>Either Http code 404 (NotFound) or Http code 200 (Ok)</returns>
     [HttpGet("/msgs/{username}")]
-    public IActionResult GetMessages([FromQuery]int latest, string username)
+    public IActionResult GetMessages([FromQuery] int latest, string username, [FromQuery] int no = 100)
     {
         LatestDBUtils.UpdateLatest(_context, latest);
-       
+
         User? profileUser = GetUser(username);
-        if (!IsLoggedIn()){ return NotFound(); } // maybe should be Unauthorized();
+        if (!IsLoggedIn()) { return NotFound(); } // maybe should be Unauthorized();
 
         var messages = (from message in _context.Messages
                         join user in _context.Users on message.AuthorId equals user.UserId
                         where user.UserId == profileUser.UserId
                         orderby message.PubDate descending
-                        select new MessageAuthor { Message = message, Author = user }).Take(30).ToList();
+                        select new { Content = message.Text, message.PubDate, User = user.Username }).Take(no).ToList();
 
         return Ok(messages);
     }
@@ -133,17 +134,17 @@ public class TimelineController : Controller
     /// <summary>
     /// Gets all messages in the database
     /// </summary>
-    /// <returns>Http code 200 (Ok)</returns>   
+    /// <returns>Http code 200 (Ok)</returns>
     [HttpGet("/msgs")]
-    public IActionResult GetAllMessages([FromQuery]int latest)
+    public IActionResult GetAllMessages([FromQuery] int latest, [FromQuery] int no = 100)
     {
         LatestDBUtils.UpdateLatest(_context, latest);
-        
+
         var messages = (from message in _context.Messages
                         join user in _context.Users on message.AuthorId equals user.UserId
                         where message.Flagged == 0
                         orderby message.PubDate descending
-                        select new MessageAuthor { Message = message, Author = user }).Take(30).ToList();
+                        select new { Content = message.Text, message.PubDate, User = user.Username }).Take(no).ToList();
 
         return Ok(messages);
     }
@@ -154,19 +155,19 @@ public class TimelineController : Controller
     /// <param name="username">The username of the user, whose follows should be returned.</param>
     /// <returns>Either http code 404 (NotFound) or http code 200 (Ok)</returns>
     [HttpGet("fllws/{username}")]
-    public IActionResult GetFollows([FromQuery]int latest, string username)
+    public IActionResult GetFollows([FromQuery] int latest, string username)
     {
         LatestDBUtils.UpdateLatest(_context, latest);
-        
+
         User? profileUser = GetUser(username);
-        if (!IsLoggedIn()){ return NotFound(); } // maybe should be Unauthorized();
+        if (!IsLoggedIn()) { return NotFound(); } // maybe should be Unauthorized();
 
         var follows = (from user in _context.Users
-                       join follower in _context.Followers 
+                       join follower in _context.Followers
                        on user.UserId equals follower.WhomId
                        where follower.WhoId == profileUser.UserId
                        select user.Username).Take(100).ToList();
-              
+
         return Ok(follows);
     }
 }
