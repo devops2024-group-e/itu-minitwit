df55105327e88afece69292af176a04a50112e1a
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2024-03-03 16:28:05 +0100

    Remove flag tool code and references (#96)

--- Makefile
+++ Makefile
@@ -1,12 +1,6 @@
 init:
 	sh ./control.sh init
 
-build:
-	gcc flag_tool.c -l sqlite3 -o flag_tool
-
-clean:
-	rm flag_tool
-
 test:
 	python3 minitwit_tests.py
 
--- control.sh
+++ control.sh
@@ -8,17 +8,17 @@ if [ "$1" = "init" ]; then
     fi
     echo "Putting a database to ./test-tmp/minitwit.db..."
     (cd Minitwit && dotnet ef database update)
-    
+
 elif [ "$1" = "start" ]; then
     echo "Starting minitwit..."
     nohup "$(which python3)" minitwit.py > /tmp/out.log 2>&1 &
 elif [ "$1" = "stop" ]; then
     echo "Stopping minitwit..."
     pkill -f minitwit
-elif [ "$1" = "inspectdb" ]; then
-    ./flag_tool -i | less
-elif [ "$1" = "flag" ]; then
-    ./flag_tool "$@"
+# elif [ "$1" = "inspectdb" ]; then
+#     ./flag_tool -i | less
+# elif [ "$1" = "flag" ]; then
+#     ./flag_tool "$@"
 else
   echo "I do not know this command..."
 fi
