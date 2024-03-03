575f0f76f979c7d01e02a929c631183def9af4a5
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2024-03-03 13:15:27 +0100

    Change input to use bash string interpolation (#107)

--- remote-server/deploy.sh
+++ remote-server/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo $DOCKER_PASSWORD | docker login ghcr.io -u $DOCKER_USERNAME --password-stdin
+docker login ghcr.io -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 docker run --platform linux/amd64 -p 80:80 -d ghcr.io/devops2024-group-e/frontend.minitwit
 docker run --platform linux/amd64 -p 8080:8080 -d ghcr.io/devops2024-group-e/backend.minitwit
