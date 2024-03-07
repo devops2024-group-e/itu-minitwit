2860a3db459842e40d5dee39f8a949b13321fc0c
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2024-03-07 19:46:46 +0100

    Add unfollow and follow test helper functions

--- Minitwit.Simulator.Api.Tests/Utils/MinitwitApiHttpClientExtensions.cs
+++ Minitwit.Simulator.Api.Tests/Utils/MinitwitApiHttpClientExtensions.cs
@@ -18,4 +18,10 @@ public static class MinitwtiApiHttpClientExtensions
 
     public static async Task<HttpResponseMessage> GetLatestUserMessagesAsync(this HttpClient client, string username, int messageCount, int latest)
         => await client.GetAsync($"/msgs/{username}?latest={latest}&no={messageCount}");
+
+    public static async Task<HttpResponseMessage> FollowUserAsync(this HttpClient client, string currentUser, string usernameToFollow, int latest)
+        => await client.PostAsJsonAsync($"/fllws/{currentUser}?latest={latest}", new { Follow = usernameToFollow });
+
+    public static async Task<HttpResponseMessage> UnfollowUserAsync(this HttpClient client, string currentUser, string usernameToUnFollow, int latest)
+        => await client.PostAsJsonAsync($"/fllws/{currentUser}?latest={latest}", new { Unfollow = usernameToUnFollow });
 }
