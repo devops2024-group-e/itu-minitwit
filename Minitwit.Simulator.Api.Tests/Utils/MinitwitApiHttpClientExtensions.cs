fef95e48b92159c5fd150e6b628cd2c111ba0c59
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2024-03-07 17:54:25 +0100

    Add common api call extension methods

---
+++ Minitwit.Simulator.Api.Tests/Utils/MinitwitApiHttpClientExtensions.cs
@@ -0,0 +1,21 @@
+namespace Minitwit.Simulator.Api.Tests;
+
+public static class MinitwtiApiHttpClientExtensions
+{
+    public static async Task<HttpResponseMessage> RegisterUserAsync(this HttpClient client, string username, string email, string password, int latest)
+        => await client.PostAsJsonAsync($"/register?latest={latest}", new
+        {
+            Username = username,
+            Email = email,
+            Pwd = password
+        });
+
+    public static async Task<HttpResponseMessage> CreateMessage(this HttpClient client, string content, string username, int latest)
+        => await client.PostAsJsonAsync($"/msgs/{username}?latest={latest}", new { Content = content });
+
+    public static async Task<HttpResponseMessage> GetLatestMessages(this HttpClient client, int messageCount, int latest)
+        => await client.GetAsync($"/msgs?latest={latest}&no={messageCount}");
+
+    public static async Task<HttpResponseMessage> GetLatestUserMessages(this HttpClient client, string username, int messageCount, int latest)
+        => await client.GetAsync($"/msgs/{username}?latest={latest}&no={messageCount}");
+}
