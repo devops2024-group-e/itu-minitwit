b584812656a108eb8cbce40acb705806da2145c2
amalieboegild <abso@itu.dk>
2024-03-07 14:47:45 +0100

    Add tests for MessageRepository and UserRepository

--- Minitwit.Infrastructure.Tests/Repositories/MessageRepositoryTests.cs
+++ Minitwit.Infrastructure.Tests/Repositories/MessageRepositoryTests.cs
@@ -1,10 +1,69 @@
-namespace Minitwit.Infrastructure.Tests.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Minitwit.Infrastructure.Repositories;
+using Minitwit.Infrastructure;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Minitwit.Infrastructure.Models;
 
 public class MessageRepositoryTests
 {
-    [Fact]
-    public void Test1()
-    {
+    private readonly IMessageRepository _messageRepo;
 
+    private readonly User _user1;
+    public MessageRepositoryTests()
+    {
+        var builder = new DbContextOptionsBuilder<MinitwitContext>();
+        builder.UseInMemoryDatabase("MinitwitUserTestDB");
+        var context = new MinitwitContext(builder.Options);
+        context.Database.EnsureCreated();
+
+        context.Messages.Add(new Message{MessageId = 1, AuthorId = 1, Text = "hello guys", PubDate = 1, Flagged = 0});
+        context.Messages.Add(new Message{MessageId = 2, AuthorId = 2, Text = "Nice weather today.", PubDate = 2, Flagged = 0});
+        context.Messages.Add(new Message{MessageId = 3, AuthorId = 3, Text = "I am flagged", PubDate = 3, Flagged = 1});
+
+        _user1 = new User{UserId = 1, Email = "1@g.dk", Username = "user1", PwHash = "abcd"};
+        context.Users.Add(_user1);
+
+        _messageRepo = new MessageRepository(context);
+
+        context.SaveChanges();
+    }
+
+    [Fact]
+    public void AddMessageReturnsTrue()
+    {
+        var response = _messageRepo.AddMessage("Hello", 2);
+
+        Assert.True(response);
+    }
+
+    [Fact]
+    public void GetMessagesReturnsRightMessages()
+    {
+        var response = _messageRepo.GetMessages();
+
+        Assert.Equal(3, response.Count);
+        Assert.Equal(1, response[0].Message.MessageId);
+        Assert.Equal(2, response[1].Message.MessageId);
+        Assert.Equal(3, response[2].Message.MessageId);
+    }
+
+    [Fact]
+    public void GetUserSpecificMessagesReturnsRightMessages()
+    {
+        var response = _messageRepo.GetUserSpecificMessages(_user1);
+
+        Assert.Equal(1, response.Count);
+        Assert.Equal(1, response[0].Message.MessageId);
+        Assert.Equal("hello guys", response[0].Message.Text);
+    }
+
+    [Fact]
+    public void GetCurrentUserSpecificMessagesReturnsRightMessages()
+    {
+        var response = _messageRepo.GetCurrentUserSpecificMessages(1);
+
+        Assert.Equal(1, response.Count);
+        Assert.Equal(1, response[0].Message.MessageId);
+        Assert.Equal("hello guys", response[0].Message.Text);
     }
 }
--- Minitwit.Infrastructure.Tests/Repositories/UserRepositoryTests.cs
+++ Minitwit.Infrastructure.Tests/Repositories/UserRepositoryTests.cs
@@ -1,10 +1,71 @@
+using Microsoft.EntityFrameworkCore;
+using Minitwit.Infrastructure.Repositories;
+
 namespace Minitwit.Infrastructure.Tests.Repositories;
 
 public class UserRepositoryTests
 {
-    [Fact]
-    public void Test1()
-    {
 
+    private readonly IUserRepository _userRepo;
+    public UserRepositoryTests()
+    {
+        var builder = new DbContextOptionsBuilder<MinitwitContext>();
+        builder.UseInMemoryDatabase("MinitwitUserTestDB");
+        var context = new MinitwitContext(builder.Options);
+        context.Database.EnsureCreated();
+
+        context.Users.Add(new Models.User{UserId = 1, Email = "1@g.dk", Username = "user1", PwHash = "abcd"});
+        context.Users.Add(new Models.User{UserId = 2, Email = "2@g.dk", Username = "user2", PwHash = "efgh"});
+        context.Users.Add(new Models.User{UserId = 3, Email = "3@g.dk", Username = "user3", PwHash = "ijkl"});
+
+        _userRepo = new UserRepository(context);
+
+        context.SaveChanges();
+    }
+
+    [Fact]
+    public void GetUserFromUsernameReturnsRightUser()
+    {
+        var response = _userRepo.GetUser("user1");
+
+        Assert.Equal(1, response.UserId);
+        Assert.Equal("1@g.dk", response.Email);
+        Assert.Equal("user1", response.Username);
+        Assert.Equal("abcd", response.PwHash);
+    }
+
+    [Fact]
+    public void GetUserFromIdReturnsRightUser()
+    {
+        var response = _userRepo.GetUser(2);
+
+        Assert.Equal(2, response.UserId);
+        Assert.Equal("2@g.dk", response.Email);
+        Assert.Equal("user2", response.Username);
+        Assert.Equal("efgh", response.PwHash);
+    }
+
+    [Fact]
+    public void DoesUsereExistReturnsTrue()
+    {
+        var response = _userRepo.DoesUserExist("user3");
+
+        Assert.True(response);
+    }
+
+    [Fact]
+    public void DoesUsereExistReturnsFalse()
+    {
+        var response = _userRepo.DoesUserExist("user4");
+
+        Assert.False(response);
+    }
+
+    [Fact]
+    public void AddUserReturnsTrue()
+    {
+        var response = _userRepo.AddUser("newUser", "adsj@ad.dk", "szdhi");
+
+        Assert.True(response);
     }
 }
