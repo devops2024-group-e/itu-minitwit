a26dd907a70a55958e2d1575492b3c7418be8342
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2024-03-07 20:51:12 +0100

    Remove dropping of tables

--- schema.sql
+++ schema.sql
@@ -1,4 +1,3 @@
-drop table if exists "user";
 create table "user" (
   user_id serial primary key,
   username varchar(100) not null,
@@ -6,14 +5,12 @@ create table "user" (
   pw_hash varchar(200) not null
 );
 
-drop table if exists follower;
 create table follower (
   who_id integer,
   whom_id integer,
   PRIMARY KEY (who_id, whom_id)
 );
 
-drop table if exists message;
 create table message (
   message_id serial primary key,
   author_id integer not null,
@@ -22,7 +19,6 @@ create table message (
   flagged integer
 );
 
-drop table if exists "latest";
 create table latest (
   id serial primary key,
   command_id integer not null
