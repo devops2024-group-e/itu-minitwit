69d072f357330896d49019b363eb3a7350065a36
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2024-03-07 09:44:41 +0100

    Add login logout test

--- Minitwit.Tests/IntegrationTests/LoginAndLogoutTests.cs
+++ Minitwit.Tests/IntegrationTests/LoginAndLogoutTests.cs
@@ -13,9 +13,28 @@ public class MinitwitUserInteraction : IClassFixture<MinitwitApplicationFactory<
         _factory = factory;
         _client = factory.CreateClient();
 
-        var createUserTask = _client.CreateTestUserAsync();
+        using (var setupScope = _factory.Services.CreateScope())
+        {
+            var context = setupScope.ServiceProvider.GetService<MinitwitContext>();
+
+            var createUserTask = _client.CreateTestUserAsync("UserInteractionUser1");
         createUserTask.Wait();
     }
+    }
 
+    [Fact]
+    public async Task LoginAndLogout_WithCorrectCredentials_ReturnsCorrectMessages()
+    {
+        var content = new FormUrlEncodedContent(new Dictionary<string, string> { { "username", "UserInteractionUser1" }, { "password", "default" } });
 
+        var response = await _client.PostAsync("/login", content);
+        var responseText = await response.Content.ReadAsStringAsync();
+
+        Assert.Contains("You were logged in", responseText);
+
+        var logoutResponse = await _client.PostAsync("/logout", new FormUrlEncodedContent(Enumerable.Empty<KeyValuePair<string, string>>()));
+        var logoutResponseText = await logoutResponse.Content.ReadAsStringAsync();
+
+        Assert.Contains("You were logged out", logoutResponseText);
+    }
 }
