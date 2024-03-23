3176478f08488932ea85246b3fe8e6b87e639495
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2024-03-23 17:50:21 +0100

    Remove non-async functions and add code documentation

--- Minitwit.Infrastructure/Repositories/IMessageRepository.cs
+++ Minitwit.Infrastructure/Repositories/IMessageRepository.cs
@@ -2,21 +2,39 @@ using Minitwit.Infrastructure.Models;
 
 namespace Minitwit.Infrastructure.Repositories;
 
+/// <summary>
+/// Represents operations to get, delete or change Messages from a data store
+/// </summary>
 public interface IMessageRepository
 {
-    bool AddMessage(string text, int authorId);
-
-    List<MessageAuthor> GetMessages(int no);
-
-    List<MessageAuthor> GetUserSpecificMessages(User profileUser, int no);
-
-    List<MessageAuthor> GetCurrentUserSpecificMessages(int currentUserId, int no);
-
+    /// <summary>
+    /// Adds a message with an author id
+    /// </summary>
+    /// <param name="text">The text of the message</param>
+    /// <param name="authorId">The user id of the author of the message</param>
+    /// <returns>True if the message is added, false if not</returns>
     Task<bool> AddMessageAsync(string text, int authorId);
 
+    /// <summary>
+    /// Gets all messages
+    /// </summary>
+    /// <param name="no">The maximum nomber of messages to return</param>
+    /// <returns>Returns a list of all messages with a maximum of no</returns>
     Task<List<MessageAuthor>> GetMessagesAsync(int no);
 
+    /// <summary>
+    /// Get user specific messages
+    /// </summary>
+    /// <param name="profileUser">The user who posted the messages</param>
+    /// <param name="no">The maximum amount of messages to return</param>
+    /// <returns>Returns a list of all messages with a maximum of no</returns>
     Task<List<MessageAuthor>> GetUserSpecificMessagesAsync(User profileUser, int no);
 
+    /// <summary>
+    /// Gets the current users posts
+    /// </summary>
+    /// <param name="currentUserId">Id of the current user</param>
+    /// <param name="no">The maximum number of messages to return</param>
+    /// <returns>Returns a list of all messages with a maximum of no</returns>
     Task<List<MessageAuthor>> GetCurrentUserSpecificMessagesAsync(int currentUserId, int no);
 }
--- Minitwit.Infrastructure/Repositories/IUserRepository.cs
+++ Minitwit.Infrastructure/Repositories/IUserRepository.cs
@@ -2,21 +2,41 @@ using Minitwit.Infrastructure.Models;
 
 namespace Minitwit.Infrastructure.Repositories;
 
+/// <summary>
+/// Represents operations to get, delete or change Users from a data store
+/// </summary>
 public interface IUserRepository
 {
-    User? GetUser(string username);
-
-    User GetUser(int currentUserId);
-
-    bool DoesUserExist(string username);
-
-    bool AddUser(string username, string email, string password);
-
+    /// <summary>
+    /// Gets the user by username
+    /// </summary>
+    /// <param name="username">Username to get the user from</param>
+    /// <returns></returns>
     Task<User?> GetUserAsync(string username);
 
+    /// <summary>
+    /// Gets the user by id
+    /// </summary>
+    /// <param name="currentUserId">The id of the user to get</param>
+    /// <returns>
+    /// The user with particular id.
+    /// If null then it means it could not find the user
+    /// </returns>
     Task<User?> GetUserAsync(int currentUserId);
 
+    /// <summary>
+    /// Checks if a user with the given username exists in the datastore
+    /// </summary>
+    /// <param name="username">The username to look for</param>
+    /// <returns>True if a user with the username exists, and false if not</returns>
     Task<bool> DoesUserExistAsync(string username);
 
+    /// <summary>
+    /// Adds a user to the datastore
+    /// </summary>
+    /// <param name="username">Username of the user</param>
+    /// <param name="email">Email of the user</param>
+    /// <param name="password">Password of the user</param>
+    /// <returns>True if the user is added succesfully</returns>
     Task<bool> AddUserAsync(string username, string email, string password);
 }
