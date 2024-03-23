7b73f23a9db924164975b9491b5fa53f50a9fd87
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2024-03-23 17:19:31 +0100

    Add initial pulumi setup

---
+++ Minitwit.Provision/Infrastructure/DigitalOcean/DOVirtualMachine.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Pulumi;
+using Pulumi.DigitalOcean;
+
+namespace Minitwit.Provision.Infrastructure.DigitalOcean;
+
+/// <summary>
+/// Represents a virtual machine in digitalocean. This is also reffered to as a Droplet in DigitalOcean terms.
+/// </summary>
+internal record DOVirtualMachine : IVirtualMachine
+{
+    Output<string> IVirtualMachine.Name => _virtualMachine.Name;
+
+    Output<string> IVirtualMachine.OS => _virtualMachine.Image;
+
+    Output<string> IVirtualMachine.Region => _virtualMachine.Region;
+
+    IDictionary<string, Output<string>> IVirtualMachine.NetworkInterfaces => _networkInterfaces;
+
+
+    private readonly Droplet _virtualMachine;
+
+    private readonly IDictionary<string, Output<string>> _networkInterfaces;
+
+    /// <summary>
+    /// Creates a new instance of a Digital Ocean virtual machine
+    /// </summary>
+    /// <param name="name">Name of the virtual machine</param>
+    /// <param name="os">The operating system and version.
+    /// You can find the list of available operating system images here https://docs.digitalocean.com/products/droplets/details/images/
+    /// </param>
+    /// <param name="region">The region where the virtual machine should be placed.
+    /// The provided name should be the slug name.
+    /// You can find the list of available regions here https://docs.digitalocean.com/products/platform/availability-matrix/#app-platform-availability
+    /// </param>
+    public DOVirtualMachine(string name, string os, string region)
+    {
+        _virtualMachine = new Droplet(name, new()
+        {
+            Image = os,
+            Region = region,
+            Size = "s-1vcpu-1gb",
+        });
+
+        _networkInterfaces = new Dictionary<string, Output<string>>
+        {
+            {"PublicIP",  _virtualMachine.Ipv4Address},
+            {"PrivateIP", _virtualMachine.Ipv4AddressPrivate},
+            {"PublicIPv6", _virtualMachine.Ipv6Address},
+        };
+    }
+
+    /// <summary>
+    /// Creates a new instance of a Digital Ocean virtual machine
+    /// </summary>
+    /// <param name="name">Name of the virtual machine</param>
+    /// <param name="os">The operating system and version.
+    /// You can find the list of available operating system images here https://docs.digitalocean.com/products/droplets/details/images/
+    /// </param>
+    /// <param name="region">The region where the virtual machine should be placed.
+    /// The provided name should be the slug name.
+    /// You can find the list of available regions here https://docs.digitalocean.com/products/platform/availability-matrix/#app-platform-availability
+    /// </param>
+    /// <param name="sshKeys">List of public ssh keys that should be able to be authorized</param>
+    public DOVirtualMachine(string name, string os, string region, InputList<string> sshKeys)
+    {
+        _virtualMachine = new Droplet(name, new()
+        {
+            Image = os,
+            Region = region,
+            Size = "s-1vcpu-1gb",
+            SshKeys = sshKeys
+        });
+
+        _networkInterfaces = new Dictionary<string, Output<string>>
+        {
+            {"PublicIP",  _virtualMachine.Ipv4Address},
+            {"PrivateIP", _virtualMachine.Ipv4AddressPrivate},
+            {"PublicIPv6", _virtualMachine.Ipv6Address},
+        };
+    }
+
+    /// <summary>
+    /// Creates a Virtual Machine a.k.a. droplet
+    /// </summary>
+    /// <param name="name">Name of the virtual machine</param>
+    /// <param name="os">The operating system and version. You can find the list of available operating system images here https://docs.digitalocean.com/products/droplets/details/images/</param>
+    /// <param name="region">The region where the virtual machine should be placed</param>
+    /// <returns>Returns the virtual machine</returns>
+    public static IVirtualMachine CreateVM(string name, string os, string region) => new DOVirtualMachine(name, os, region);
+
+    /// <summary>
+    /// Creates a set of similar virtual machines
+    /// </summary>
+    /// <param name="setNamePrefix">The prefix name of the set of virtual machines</param>
+    /// <param name="os">The operating system and version. You can find the list of available operating system images here https://docs.digitalocean.com/products/droplets/details/images/</param>
+    /// <param name="region">The region where the virtual machine should be placed</param>
+    /// <param name="count">The number of virtual machines to be created</param>
+    /// <returns>Returns an enumerable of virtual machines</returns>
+    public static IEnumerable<IVirtualMachine> CreateVMSet(string setNamePrefix, string os, string region, int count)
+    {
+        string setName = setNamePrefix[^1] == '-' ? setNamePrefix : $"{setNamePrefix}-"; // Create the prefix seperator
+
+        // Creates a collection of VM's with the following naming scheme: <VM Set Name>-<VM Number>
+        return Enumerable.Range(1, count).Select(vmNumber => CreateVM($"{setName}{vmNumber}", os, region));
+    }
+}
---
+++ Minitwit.Provision/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Pulumi;
+using Minitwit.Provision.Infrastructure;
+using Minitwit.Provision.Infrastructure.DigitalOcean;
+using Pulumi.DigitalOcean;
+
+const string VM_IMAGE = "ubuntu-20-04-x64";
+const string REGION = "fra1";
+
+return await Deployment.RunAsync(() =>
+{
+    SshKey andreasSSHKey = new("Andreas-ssh-key", new SshKeyArgs
+    {
+        Name = "Andreas-ssh-key",
+        PublicKey = "Abe"
+    });
+
+    // Create the web servers
+    IEnumerable<IVirtualMachine> webServers = DOVirtualMachine.CreateVMSet("minitwit-web-test", VM_IMAGE, REGION, count: 3);
+
+    // Create the monitoring servers
+    IVirtualMachine monitoringServer = DOVirtualMachine.CreateVM("minitwit-mon-test-1", VM_IMAGE, REGION);
+
+    // Export outputs here
+    return new Dictionary<string, object?>
+    {
+        ["web-server-ips"] = webServers.Select(x => x.NetworkInterfaces["PublicIP"]).ToList(),
+        ["monitoring-server-ips"] = monitoringServer.NetworkInterfaces["PublicIP"]
+    };
+});
