35de2de1db0e5b396054e7374ca0506d5bca26d6
Pinkvinus <mille.mei.zhen.loo@gmail.com>
2024-04-05 13:32:40 +0000

    Added support for different password hashes

--- MinitwitSimulatorAPI/Utils/PasswordHash.cs
+++ MinitwitSimulatorAPI/Utils/PasswordHash.cs
@@ -8,27 +8,53 @@ public static class PasswordHash
     public static string Hash(string value)
     {
         var salt = RandomNumberGenerator.GetBytes(16);
-        return GeneratePasswordHash(value, salt);
+        return GeneratePasswordHash(value, salt, "SHA512"); // TODO: Change back to 512
     }
 
-    private static string GeneratePasswordHash(string password, byte[] salt)
+    private static string GeneratePasswordHash(string password, byte[] salt, string algorithm)
     {
-        var hashedResult = Convert.ToHexString(KeyDerivation.Pbkdf2(
+        string hashedResult;
+        string result;
+        switch(algorithm)
+        {
+            case "SHA512":
+                hashedResult = Convert.ToHexString(KeyDerivation.Pbkdf2(
                     password: password,
                     salt: salt,
                     prf: KeyDerivationPrf.HMACSHA512,
-                    iterationCount: 100000,
+                    iterationCount: 21000,
                     numBytesRequested: 32));
-        return $"{Convert.ToHexString(salt)}${hashedResult}";
+                result = $"{Convert.ToHexString(salt)}${hashedResult}";
+                break;
+            default:
+                hashedResult = Convert.ToHexString(KeyDerivation.Pbkdf2(
+                    password: password,
+                    salt: salt,
+                    prf: KeyDerivationPrf.HMACSHA256,
+                    iterationCount: 600000,
+                    numBytesRequested: 32));
+                result = $"SHA512${Convert.ToHexString(salt)}${hashedResult}";
+                break;
+        }
+        
+        return result;
     }
 
     public static bool CheckPasswordHash(string password, string hashedPassword)
     {
         var passwordParts = hashedPassword.Split('$');
 
-        if (passwordParts.Length != 2)
-            return false;
-        var pp = GeneratePasswordHash(password, Convert.FromHexString(passwordParts[0]));
+        string pp;
+
+        switch(passwordParts[0])
+        {
+            case "SHA512":
+                pp = GeneratePasswordHash(password, Convert.FromHexString(passwordParts[1]), "SHA512");
+                break;
+            default:
+                pp = GeneratePasswordHash(password, Convert.FromHexString(passwordParts[0]), "SHA256");
+                break;
+        }
         return pp == hashedPassword;
     }
 }
