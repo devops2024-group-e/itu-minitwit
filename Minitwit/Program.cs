a2bd0f1d7e808d149b2efdc0f754f6f8ebe7b1ed
amalieboegild <abso@itu.dk>
2024-04-24 19:41:05 +0000

    Add session configuration

--- Minitwit/Program.cs
+++ Minitwit/Program.cs
@@ -35,19 +35,12 @@ builder.Services.AddOpenTelemetry()
 // Add session settings
 builder.Services.AddDistributedPostgreSqlCache(setup =>
 {
-    setup.ConnectionString = configuration["ConnectionString"];
-    setup.SchemaName = configuration["SchemaName"];
-    setup.TableName = configuration["TableName"];
-    setup.DisableRemoveExpired = configuration["DisableRemoveExpired"];
-    // Optional - DisableRemoveExpired default is FALSE
-    setup.CreateInfrastructure = configuration["CreateInfrastructure"];
-    // CreateInfrastructure is optional, default is TRUE
-    // This means que every time starts the application the
-    // creation of table and database functions will be verified.
-    setup.ExpiredItemsDeletionInterval = TimeSpan.FromMinutes(30);
-    // ExpiredItemsDeletionInterval is optional
-    // This is the periodic interval to scan and delete expired items in the cache. Default is 30 minutes.
-    // Minimum allowed is 5 minutes. - If you need less than this please share your use case 😁, just for curiosity...
+    setup.ConnectionString = builder.Configuration.GetConnectionString("MinitwitDatabase");
+    setup.SchemaName = "public";
+    setup.TableName = "session";
+    setup.DisableRemoveExpired = false;
+    setup.CreateInfrastructure = false;
+    setup.ExpiredItemsDeletionInterval = TimeSpan.FromMinutes(15);
 });
 
 builder.Services.AddSession(options =>
