10023e7c2b5de78809ed30da8d83a7bccc0b97ba
amalieboegild <abso@itu.dk>
2024-06-03 09:04:35 +0000

    Delete viewmodels

--- MinitwitSimulatorAPI/ViewModels/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MinitwitSimulatorAPI.ViewModels;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- MinitwitSimulatorAPI/ViewModels/PublicTimelineViewModel.cs
+++
@@ -1,9 +0,0 @@
-using MinitwitSimulatorAPI.Models;
-
-namespace MinitwitSimulatorAPI.ViewModels
-{
-    public class PublicTimelineViewModel
-    {
-        public required ICollection<MessageAuthor> Messages { get; set; }
-    }
-}
